932480b1de262f3f67b98340c27d3280db26580e
Ubuntu <ubuntu@ip-172-31-59-46.ec2.internal>
2021-09-10 16:26:33 +0000

    added correct Hello, World! program

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Created by: Igor
+# Created on: Sept 2021
+# This is Hello, World! programm
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main_":
+    main()
